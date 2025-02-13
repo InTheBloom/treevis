625fe915cbdb8fdf1887fd28fa7f8a31849c6624
InTheBloom@wsl <nato.rider.smm2@gmail.com>
2025-02-13 09:45:46 +0900

    canvasクラスにグラフ描画apiを実装し始める

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
 class Canvas {
     constructor (elem) {
         this.elem = elem;
+        this.margin_w = 0;
+        this.margin_h = 0;
+
+        this.radius = 10;
+
+        this.rows = 1;
+        this.cols = 1;
     }
 
     // 全てpx指定
@@ -21,9 +28,61 @@ class Canvas {
         return this.elem.height;
     }
 
+    set_margin_w (w) {
+        if (w < 0) throw new Error("set_margin_w: minus margin.");
+        this.margin_w = w;
+        this.margin_w = Math.min(this.margin_w, this.elem.width / 2);
+    }
+    set_margin_h (h) {
+        if (h < 0) throw new Error("set_margin_h: minus margin.");
+        this.margin_h = h;
+        this.margin_h = Math.min(this.margin_h, this.elem.height / 2);
+    }
+    set_radius (r) {
+        if (r < 0) throw new Error("set_radius: minus radius.");
+        this.radius = r;
+    }
+    split_canvas (cols, rows) {
+        if (cols < 0) throw new Error("split_canvas: minus cols.");
+        if (rows < 0) throw new Error("split_canvas: minus rows.");
+        this.cols = cols;
+        this.rows = rows;
+    }
+
+    lattice_to_px (x, y) {
+        if (x < 0 || this.cols < x) throw new Error("lattice_to_px: Invalid x.");
+        if (y < 0 || this.rows < y) throw new Error("lattice_to_px: Invalid y.");
+        const actual_h = this.elem.height - 2 * this.margin_h;
+        const actual_w = this.elem.width - 2 * this.margin_w;
+
+        return [
+            x * (actual_w / this.cols) + this.margin_w,
+            y * (actual_h / this.rows) + this.margin_h,
+        ];
+    }
+
+    draw_vertex (x, y, label) {
+        const cod = this.lattice_to_px(x, y);
+        const ctx = this.elem.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(
+            cod[0],
+            cod[1],
+            this.radius,
+            0, 2 * Math.PI
+        );
+        ctx.stroke();
+        ctx.font = `${this.radius}px serif`;
+        ctx.fillText(
+            `${label}`,
+            cod[0] - this.radius / 3.6,
+            cod[1] + this.radius / 2.7,
+        );
+    }
+
     clear () {
         const ctx = this.elem.getContext("2d");
-        ctx.fillRect(0, 0, this.elem.width, this.elem.height);
+        ctx.clearRect(0, 0, this.elem.width, this.elem.height);
     }
 }
 
@@ -55,7 +114,6 @@ class Graph {
             }
         }
         dfs(begin);
-        console.log(vis);
 
         return Object.keys(vis).length == Object.keys(this.adj).length;
     }
@@ -101,11 +159,12 @@ function main () {
         error_output.textContent = "";
         try {
             const graph = parse_graph_input(e.target.value);
-            if (!g.is_tree()) {
+            if (!graph.is_tree()) {
                 throw new Error("入力されたグラフが木ではありません。");
             }
 
-            // 描画アルゴリズムに渡す
+            // 画面クリア -> 描画アルゴリズムに渡す
+            canvas.clear();
             draw(canvas, graph);
         }
         catch (e) {
@@ -115,4 +174,7 @@ function main () {
 }
 
 function draw (canvas, graph) {
+    canvas.set_radius(100);
+    canvas.split_canvas(10, 10);
+    canvas.draw_vertex(2, 2, 0);
 }
